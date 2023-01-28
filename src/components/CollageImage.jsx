84103a61247601cae96ebd5d22e1017855504ed0
davidnatanael <davidnatanaelgomez2@gmail.com>
2023-01-28 15:40:46 -0300

    more

--- src/components/CollageImage.jsx
+++ src/components/CollageImage.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react'
 import style from './CollageImage.module.css'
 
 const CollageImage=({id,title,data,cssstyle, type,viewImage})=>{
+const [plus,setPlus]=useState(true)
+  
     return (
 
     <div id={id} className={style.container}>
       <h1>{title}</h1>
       <div className={style.containergrid}>
-      {data.map((e,i)=>{
+      {data.slice(0,plus == false ? data.length:4 ).map((e,i)=>{
         return(
             <div className={style[cssstyle]}>
             <img  key={i} src={e}   onClick={()=>viewImage(e,i,type)} />
@@ -15,6 +18,15 @@ const CollageImage=({id,title,data,cssstyle, type,viewImage})=>{
         )
       })}
       </div>
+      <div className={style.button}>
+        {plus == false?<button  onClick={()=>setPlus(true)} className={style.vermenos}>ver menos</button>:
+              <button  onClick={()=>setPlus(false)} className={style.vermas}>ver mas</button>
+
+        }
+   
+
+      </div>
+
     
     </div>
     )
